6e8919cf29942e56db55e4cc8a881419e9765551
Kamila fatima santos oliveira <kamila.fatima@amedigital.com>
2021-01-16 21:53:54 -0300

    feat/ crud operations management

---
+++ src/main/java/com/ccr/hackathon/backend/model/ManagementContent.java
@@ -0,0 +1,70 @@
+package com.ccr.hackathon.backend.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "adm_content")
+public class ManagementContent {
+    private Long id;
+    private String title;
+    private String description;
+    private String url;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "title", length = 255, nullable = true)
+    @Size(max = 255)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "description", length = 255, nullable = true)
+    @Size(max = 255)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "url", length = 255, nullable = true)
+    @Size(max = 255)
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public ManagementContent() {
+    }
+
+    public ManagementContent(Long id, String title, String description, String url) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+    }
+
+
+    @Override
+    public String toString() {
+        return "AdmContent [id=" + id + ", title=" + title + ", description=" + description + ", url=" + url + "]";
+    }
+
+}
